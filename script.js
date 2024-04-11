acd91991200ed2c767fa7b312d861d1e309aa460
Abhishek <109225495+evoAB@users.noreply.github.com>
2024-04-11 22:23:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function mapLetters(s) {
+	let obj={}
+	for(let x in s){
+	  if(obj[s[x]]==undefined)
+		obj[s[x]]=[x];
+		else
+		obj[s[x]].push(x);
+	}
+	return obj;
+}
+const str = prompt("Enter String : ")
+alert(JSON.stringify(mapLetters(str)))
